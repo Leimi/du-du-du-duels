89707ff4fba22ae508529d2ab32b85aef1a394ea
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-11-03 15:51:47 +0100

    adding hearthhead tooltips

--- app/views/layout/foot.php
+++ app/views/layout/foot.php
@@ -1,5 +1,6 @@
 		</div>
 
+		<script src="http://static.wowhead.com/widgets/power.js"></script>
 		<!-- dev : /js/script.js -->
 		<?php $js = PROD ? '/js/script.min.js?v=58797562341' :
 			array('/js/script.js');
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+var wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks": false };
+
 (function() {
-	
+
 })();
