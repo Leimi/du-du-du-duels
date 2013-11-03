0e7a438c8c97c4c2b2b2520f2cee166918c20110
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-11-03 18:34:24 +0100

    moar visual stuff!

--- app/views/fight.php
+++ app/views/fight.php
@@ -10,17 +10,22 @@
 			<input type="hidden" name="<?php echo $type[$fighterKey] ?>_id" value="<?php echo $fighter->id ?>" />
 
 			<div class="fighter__img-wrapper">
-				<input type="image" src="<?php echo Halp::fighterImgPath($fighter); ?>" alt="<?php echo $fighter->name ?>" name="<?php echo $type[$fighterKey] ?>_wins" class="fighter__img" value="<?php echo $fighter->name ?>" />
+				<input type="image" src="<?php echo Halp::fighterImgPath($fighter); ?>" alt="<?php echo $fighter->name ?>" name="<?php echo $type[$fighterKey] ?>_wins" title="<?php echo $fighter->name ?> wins!" class="fighter__img" value="<?php echo $fighter->name ?>" />
 			</div>
 
 			<div class="fighter__more-info">
 				<a href="http://www.hearthhead.com/card=<?php echo $fighter->hhId ?>&amp;text" target="_blank"><?php echo $fighter->name ?> <span class="icon-small-arrow-up-right" title="See this card on Hearthhead"></span></a>
 			</div>
 
-		</div><?php if ($fighterKey === 0): ?><div class="fighter__vs">vs</div><?php endif ?><?php endforeach ?>
+		</div><?php if ($fighterKey === 0): ?><div class="fight__between">
+			<div class="fight__vs-wrapper"><div class="fight__vs">vs</div></div>
+			<div class="fight__draw">
+				<input type="submit" name="draw" title="Choosing is too difficult, I call it a draw!" class="fight__draw hearthstone-button" value="Draw" />
+			</div>
+		</div><?php endif ?><?php endforeach ?>
 	</div>
 
-	<input type="submit" name="draw" class="fight__draw" value="Draw" />
+
 	<?php endif ?>
 
 	<input type="hidden" name="<?php echo $csrf_key; ?>" value="<?php echo $csrf_token; ?>">
--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,18 @@
 var wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks": false };
 
 (function() {
+	$('body').on('mouseenter', '.fighter', function() {
+		var $this = $(this).addClass('fighter--winner');
+		$('.fighter').not($this).addClass('fighter--loser');
+	});
+	$('body').on('mouseleave', '.fighter', function() {
+		$('.fighter').removeClass('fighter--loser').removeClass('fighter--winner');
+	});
 
+	$('body').on('mouseenter', '.fight__draw', function() {
+		$('.fighter').addClass('fighter--draw');
+	});
+	$('body').on('mouseleave', '.fight__draw', function() {
+		$('.fighter').removeClass('fighter--draw');
+	});
 })();
