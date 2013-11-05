e81720d24ce4ff53134f7691561a3e2faefa2970
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-11-05 13:55:50 +0100

    no french!

--- app/views/layout/head.php
+++ app/views/layout/head.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <?php $page = !empty($page) ? "page--".$page.' ' : ''; ?>
-<!--[if lt IE 7]>      <html lang="fr" class="<?php echo $page ?>no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html lang="fr" class="<?php echo $page ?>no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html lang="fr" class="<?php echo $page ?>no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang="fr" class="<?php echo $page ?>no-js "> <!--<![endif]-->
+<!--[if lt IE 7]>      <html lang="en" class="<?php echo $page ?>no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" class="<?php echo $page ?>no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" class="<?php echo $page ?>no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="<?php echo $page ?>no-js "> <!--<![endif]-->
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
