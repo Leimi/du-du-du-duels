bbd6099a27299c3424b38521f9764e08438ce060
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2014-01-29 13:46:15 +0100

    fight page: winner/loser highlight are made only on img hover

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,20 @@
 var wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks": false };
 
 (function() {
-	$('body').on('mouseenter', '.fighter', function() {
-		var $this = $(this).addClass('fighter--winner');
+	$body = $('body');
+
+	$body.on('mouseenter', '.fighter__img', function() {
+		var $this = $(this).closest('.fighter').addClass('fighter--winner');
 		$('.fighter').not($this).addClass('fighter--loser');
 	});
-	$('body').on('mouseleave', '.fighter', function() {
+	$body.on('mouseleave', '.fighter__img', function() {
 		$('.fighter').removeClass('fighter--loser').removeClass('fighter--winner');
 	});
 
-	$('body').on('mouseenter', '.fight__draw', function() {
+	$body.on('mouseenter', '.fight__draw', function() {
 		$('.fighter').addClass('fighter--draw');
 	});
-	$('body').on('mouseleave', '.fight__draw', function() {
+	$body.on('mouseleave', '.fight__draw', function() {
 		$('.fighter').removeClass('fighter--draw');
 	});
 })();
