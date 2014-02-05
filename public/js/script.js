7045e2e63f401088ecb3424e57f89221e51b77b6
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2014-02-05 13:30:05 +0100

    adding tooltips to the top page

--- public/js/script.js
+++ public/js/script.js
@@ -30,4 +30,24 @@ var wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks"
 		});
 	}
 
+
+	if ( $('html.page--fighters').length ) {
+		function handleTooltip(e, content) {
+			if (!content)
+				content = $(e.currentTarget).attr('data-tooltip');
+			if (e.type == "mouseover")
+				MouseTooltip.show( content );
+			else if (e.type == "click")
+				MouseTooltip.html( content );
+			else
+				MouseTooltip.hide();
+		}
+		MouseTooltip.init({ "3d": false });
+		$body.on('mouseover click mouseout', '.top-item__position', function(e) {
+			handleTooltip(e, $(e.currentTarget).find('[data-tooltip]').attr('data-tooltip') );
+	}	);
+		$body.on('mouseover click mouseout', '.top-item__ratio', function(e) {
+			handleTooltip(e);
+		});
+	}
 })();
