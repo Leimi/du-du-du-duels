57314be79a10f9fe4b20d42f494613dc38860229
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2014-02-05 13:29:40 +0100

    putting top-item in a separate php file

---
+++ app/views/elements/top-item.php
@@ -0,0 +1,27 @@
+<tr>
+
+	<td data-rank="<?php echo $rank ?>" data-score="<?php echo $fighter->score ?>" title="Score: <?php echo $fighter->score ?>"><?php echo $rank.'.' ?></td>
+
+	<?php if (!is_null($diff)): ?>
+	<td data-position="<?php echo $diff ?>" class="top-item__position"><?php echo Halp::rankIcon($diff) ?></td>
+	<?php else: ?>
+	<td></td>
+	<?php endif ?>
+
+	<td data-fights="<?php echo $fighter->fights ?>">
+		<div class="progress top-item__ratio" data-tooltip="
+			<?php echo $fighter->fights.' '.Halp::pluralize('fight', $fighter->fights).'<br>'.
+				round($fighter->wins / $fighter->fights * 100).'% wins<br>'.
+				round($fighter->losts / $fighter->fights * 100).'% defeats<br>'.
+				round($fighter->draws / $fighter->fights * 100).'% draws'; ?>">
+			<div class="progress__bar progress__bar--win" style="width: <?php echo $fighter->wins / $fighter->fights * 100 ?>%"></div>
+			<div class="progress__bar progress__bar--lost" style="width: <?php echo $fighter->losts / $fighter->fights * 100 ?>%"></div>
+			<div class="progress__bar progress__bar--draw" style="width: <?php echo $fighter->draws / $fighter->fights * 100 ?>%"></div>
+		</div>
+	</td>
+
+	<td data-id="<?php echo $fighter->id ?>">
+		<a style='background-image: url("<?php echo Halp::fighterImgPath($fighter, array('thumb' => true)); ?>")' class="top-item__card-preview" href="http://www.hearthhead.com/card=<?php echo $fighter->hhId ?>" target="_blank"><?php echo $fighter->name ?></a>
+	</td>
+
+</tr>
--- app/views/top.php
+++ app/views/top.php
@@ -1,7 +1,7 @@
 <?php if (!$app->request()->isAjax()) include(__DIR__ . '/layout/head.php'); ?>
 
 <?php if ($remaining > 0): ?>
-<p class="warning">Currently, less than <?php echo round($count, ($count > 100 ? -2 : -1)) ?> duels occured in total. That's not much: the top 50 can change rather quickly.</p>
+<p class="warning">Currently, less than <?php echo round($count, ($count > 100 ? -2 : -1)) ?> duels occured in total. That's not much: the charts can change rather quickly.</p>
 <?php endif ?>
 
 <table class="top striped">
@@ -10,39 +10,19 @@
 		<th>Ratio</th>
 		<th>Card</th>
 	</tr>
-	<?php if (!empty($fighters)):
-	$i = $rank = 1;
-	$prevScore = null;
-	foreach ($fighters as $key => $fighter):
-		$diff = isset($ranks->diff[$key]) ? $ranks->diff[$key] : null;
-		$rank = $prevScore === $fighter->score ? $rank : $i; ?>
-	<tr>
-
-		<td data-rank="<?php echo $rank ?>" data-score="<?php echo $fighter->score ?>" title="Score: <?php echo $fighter->score ?>"><?php echo $rank.'.' ?></td>
-
-		<?php if (!is_null($diff)): ?>
-		<td data-position="<?php echo $diff ?>"><?php echo Halp::rankIcon($diff) ?></td>
-		<?php else: ?>
-		<td></td>
-		<?php endif ?>
-
-		<td data-fights="<?php echo $fighter->fights ?>">
-			<div class="progress">
-				<div class="progress__bar progress__bar--win" style="width: <?php echo $fighter->wins / $fighter->fights * 100 ?>%"></div>
-				<div class="progress__bar progress__bar--draw" style="width: <?php echo $fighter->draws / $fighter->fights * 100 ?>%"></div>
-				<div class="progress__bar progress__bar--lost" style="width: <?php echo $fighter->losts / $fighter->fights * 100 ?>%"></div>
-			</div>
-		</td>
-
-		<td data-id="<?php echo $fighter->id ?>">
-			<a style='background-image: url("<?php echo Halp::fighterImgPath($fighter, array('thumb' => true)); ?>")' class="top-item__card-preview" href="http://www.hearthhead.com/card=<?php echo $fighter->hhId ?>" target="_blank"><?php echo $fighter->name ?></a>
-		</td>
-
-	</tr>
 	<?php
-		$prevScore = $fighter->score;
-		$i++;
-	endforeach; endif; ?>
+	if (!empty($fighters)) {
+		$i = $rank = 1;
+		$prevScore = null;
+		foreach ($fighters as $key => $fighter) {
+			$diff = isset($ranks->diff[$key]) ? $ranks->diff[$key] : null;
+			$rank = $prevScore === $fighter->score ? $rank : $i;
+			include(__DIR__ . '/elements/top-item.php');
+			$prevScore = $fighter->score;
+			$i++;
+		}
+	}
+	?>
 </table>
 
 <?php if (!$app->request()->isAjax()) include(__DIR__ . '/layout/foot.php'); ?>
