93f15b7ffd72e79b6006032e4d2d550e92f4d5ab
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2014-02-18 00:36:14 +0100

    bower: updating jquery repo+adding mousetooltip

--- app/views/layout/default.php
+++ app/views/layout/default.php
@@ -51,7 +51,7 @@
 		<script src="http://static.wowhead.com/widgets/power.js"></script>
 		<!-- dev : /js/script.js -->
 		<?php $this->js = PROD ? '/js/script.min.js?v=58797562341' :
-			array('/components/jquery/jquery.js', '/js/mousetooltip.js', '/js/script.js');
+			array('/components/jquery/dist/jquery.js', '/components/mousetooltip/mousetooltip.js', '/js/script.js');
 		$t = time();
 		if (is_string($this->js)): ?>
 		<script src="<?php echo $this->js ?>"></script>
